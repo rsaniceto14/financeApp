051a42b93b0a5b96d3ab2b3e9dafb0d65e229f81
rsaniceto14 <raphaelaniceto14@gmail.com>
2026-02-24 20:16:17 -0300

    [feat] config auth

--- financeApp/backend/dist/src/auth/auth.service.d.ts
+++ financeApp/backend/dist/src/auth/auth.service.d.ts
@@ -1,2 +1,11 @@
+import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
 export declare class AuthService {
+    private prisma;
+    private jwtService;
+    constructor(prisma: PrismaService, jwtService: JwtService);
+    login(email: string, password: string): Promise<{
+        accessToken: string;
+        refreshToken: string;
+    }>;
 }
--- financeApp/backend/src/auth/auth.service.ts
+++ financeApp/backend/src/auth/auth.service.ts
@@ -1,4 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+import * as bcrypt from 'bcrypt'
+import { JwtService } from '@nestjs/jwt';
+
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor(private prisma: PrismaService, private jwtService: JwtService) { }
+
+    async login(email: string, password: string) {
+
+        const user = await this.prisma.user.findUnique({
+            where: { email },
+            include: {
+                memberships: true,
+            },
+        })
+
+        if (!user) throw new UnauthorizedException()
+
+        const passwordValid = await bcrypt.compare(password, user.password)
+
+        if (!passwordValid) throw new UnauthorizedException()
+
+        const membership = user.memberships[0]
+
+        const payload = {
+            sub: user.id,
+            orgId: membership.organizationId,
+            role: membership.role
+        }
+
+        const accessToken = this.jwtService.sign(payload)
+
+        const refreshToken = this.jwtService.sign(payload, {
+            expiresIn: '7d',
+        })
+
+        const hashedRefresh = await bcrypt.hash(refreshToken, 10)
+
+        await this.prisma.user.update({
+            where: { id: user.id },
+            data: { refreshToken: hashedRefresh }
+        })
+
+        return {
+            accessToken,
+            refreshToken,
+        }
+
+    }
+}
